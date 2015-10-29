95b38eb12931c0442e9bceb1377bb18817ec3886
Jonjoe Whitfield <jonjoe@Jonjoes-MacBook-Pro.local>
2015-10-29 22:36:40 +0000

    bugs

--- makeitso.sh
+++ makeitso.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc viminfo bash_profile"    # list of files/folders to symlink in homedir
+files="bashrc bash_profile vimrc viminfo "    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -22,10 +22,12 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+ln -s ~/dotfiles/vim ~/.vim
