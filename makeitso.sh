8904f3125f46a1f22ee73f0e83094a81c3608656
Jonjoe Whitfield <me@jonjoe.io>
2015-11-12 13:17:37 +0000

    garbage collection

--- makeitso.sh
+++ makeitso.sh
@@ -8,10 +8,9 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile vimrc viminfo "    # list of files/folders to symlink in homedir
+files="projects bashrc bash_profile vimrc viminfo "    # list of files/folders to symlink in homedir
 
 ##########
-
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
@@ -31,3 +30,4 @@ for file in $files; do
 done
 
 ln -s ~/dotfiles/vim ~/.vim
+rm -rf ~/dotfiles_old
