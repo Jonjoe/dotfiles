0b611f3f0045e006ddbb6eab41efd8864d4cc446
Jonjoe Whitfield <me@jonjoe.io>
2016-04-02 16:41:46 +0100

    remove defunct functions

--- autorun/terminal-happiness.sh
+++ autorun/terminal-happiness.sh
@@ -6,13 +6,6 @@ tab ()
     echo -ne "\033]0;$@\007"
 }
 
-# Improved CD
-cd ()
-{
-    builtin cd $@
-    ls
-}
-
 # Custom Commands
 alias hosts="sudo sublime /etc/hosts"
 alias homestead='function __homestead() { (cd ~/Homestead && vagrant $*); unset -f __homestead; }; __homestead'
