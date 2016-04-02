a8360150b3c3222e0125933ebfcb8eae553b17df
Jonjoe Whitfield <me@jonjoe.io>
2016-04-02 16:29:14 +0100

    defunct

--- makeitso.sh
+++ makeitso.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+dir=~/Dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="projects.sh bashrc bash_profile vimrc viminfo "    # list of files/folders to symlink in homedir
 
