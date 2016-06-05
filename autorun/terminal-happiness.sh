d8a7c81acf7e35d8b5247daf9a8c8f8ca85e5d29
Jonjoe Whitfield <jonjoe@Jonjoes-MacBook-Pro.local>
2016-06-05 02:44:50 +0100

    rails database regen function

--- autorun/terminal-happiness.sh
+++ autorun/terminal-happiness.sh
@@ -6,6 +6,13 @@ tab ()
     echo -ne "\033]0;$@\007"
 }
 
+rdb () 
+{
+	bin/rails db:drop
+	bin/rails db:create
+	bin/rails db:migrate
+	bin/rails db:seed
+}
 # Custom Commands
 alias hosts="sudo sublime /etc/hosts"
 alias homestead='function __homestead() { (cd ~/Homestead && vagrant $*); unset -f __homestead; }; __homestead'
