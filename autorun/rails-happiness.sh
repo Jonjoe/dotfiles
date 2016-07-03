c86e869a31d90bfc73ee42fc46560c05f3de1e9d
Jonjoe Whitfield <me@jonjoe.io>
2016-07-03 19:34:15 +0100

    typos

--- autorun/rails-happiness.sh
+++ autorun/rails-happiness.sh
@@ -8,9 +8,9 @@ railsDB () {
 }
 
 herokuDB () {
-	heroku pg:reset DATABASE
-	heroku run rake db:migrate
-	heroku run rake db:seedd .select-style {
+  heroku pg:reset DATABASE
+  heroku run rake db:migrate
+  heroku run rake db:seed
 }
 
 killRails () {
