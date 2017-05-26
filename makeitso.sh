d87c0920c6eba740cf996e27068c7f0fd185a35d
Jonjoe Whitfield <me@jonjoe.io>
2017-05-26 18:45:54 +0100

    major rework

--- makeitso.sh
+++ makeitso.sh
@@ -1,28 +1,29 @@
-#!/bin/bash
-
-# .makeitso.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 
-# - Variables
-DIR=~/Dotfiles                               # dotfiles directory
-OLDDIR=~/Dotfiles_old                        # old dotfiles backup directory
-FILES="zshrc tmux.conf hyper.js vimrc"       # list of files/folders to symlink in homedir
+# Variables
+ROOT=$(pwd)
 
-# change to the Dotfiles directory
-cd $DIR
+read -r -p "Are you sure? Please note: this will delete your ~/.vim folder and ~ dotfiles [y/N] " $REPLY
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-echo "Constructing Symlinks"
-for FILE in $FILES; do
-    rm -rf ~/.$FILE
-    ln -s $DIR/$FILE $HOME/.$FILE
-    echo " --- $BOLD$FILE$NORMAL"
-done
-echo "Done"
+if [[ $REPLY =~ ^[Yy]$ ]]
+  echo "Constructing Symlinks"
 
-rm -rf $HOME/.vim
-ln -s "$DIR/vim" $HOME/.vim
-rm -rf $OLDDIR
+  for FILE in $ROOT/config/* 
+  do
+    FILENAME=$(basename $FILE)
+    rm -rf ~/.$FILENAME
+
+    ln -s $FILE ~/.$FILENAME
+
+    echo "--- $BOLD$FILENAME$NORMAL"
+  done
+
+  echo "Done"
+  echo "refreshing .vim"
+
+  rm -rf ~/.vim
+  ln -s "$ROOT/vim" ~/.vim
+then
+  echo "KBai"
+fi
