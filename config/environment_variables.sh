8a16d122baa4fee0da1b7f16161b0d7ce498e5c4
Jonjoe Whitfield <me@jonjoe.io>
2017-06-26 00:33:48 +0100

    small bugs

--- config/environment_variables.sh
+++ config/environment_variables.sh
@@ -1,4 +1,4 @@
-export PG_USERNAME='postgres'
-export PG_PASSWORD="password"
-export POSTGRES_URL="postres://postgres:password@localhost:5432/midrive"
-export DOCKER_HOST=”tcp://0.0.0.0:2375
+export PG_USERNAME=postgres
+export PG_PASSWORD=password
+export POSTGRES_URL=postres://postgres:password@localhost:5432/midrive
+export DOCKER_HOST=tcp://0.0.0.0:2375
