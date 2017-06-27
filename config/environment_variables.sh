5d0436cc6864756f7c991f3ab9a9ed99f7a68372
Jonjoe Whitfield <me@jonjoe.io>
2017-06-27 10:57:43 +0100

    better env vars

--- config/environment_variables.sh
+++ config/environment_variables.sh
@@ -1,4 +1,13 @@
+# Path Updates
+		export PATH="~/.bin:$PATH"
+		export PATH="~/.rbenv/bin:$PATH"
+		export PATH="~/./node_modules/.bin:$PATH"
+		export PATH="/usr/local/sbin:$PATH"
+		export PATH="$PATH:/mnt/c/Program\ Files/Docker/Docker/resources/bin"
+# End of Path Updates
+
 export PG_USERNAME=postgres
 export PG_PASSWORD=password
+export EDITOR=vim
 export POSTGRES_URL=postres://postgres:password@localhost:5432/midrive
 export DOCKER_HOST=tcp://0.0.0.0:2375
