1b2175329ebbaff117ab6ff8234dd06a98a717a9
Jonjoe Whitfield <me@jonjoe.io>
2017-08-18 20:41:35 +0100

    more paths MOAR!!!

--- config/extra-paths.sh
+++ config/extra-paths.sh
@@ -2,14 +2,16 @@ BIN_PATH="~/.bin"
 RBENV_PATH="~/.rbenv/bin"
 NPM_GLOBAL_PATH="~/.npm-global/bin"
 SBIN_PATH="/usr/local/sbin"
+DOT_LOCAL_PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 WIN_DOCKER_PATH="/mnt/c/Program Files/Docker/Docker/resources/bin"
 BASHER_PATH="~/.basher/bin"
 
 PATH_ARRAY=(
-  $BIN_PATH 
-  $RBENV_PATH 
-  $NPM_GLOBAL_PATH 
-  $SBIN_PATH 
-  $WIN_DOCKER_PATH 
+  $BIN_PATH
+  $RBENV_PATH
+  $NPM_GLOBAL_PATH
+  $SBIN_PATH
+  $DOT_LOCAL_PATH
+  $WIN_DOCKER_PATH
   $BASHER_PATH
 )
