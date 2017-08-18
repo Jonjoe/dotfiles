482d61bc77c2d27bb69ccbeb60eec96287860845
Jonjoe Whitfield <me@jonjoe.io>
2017-08-18 20:41:55 +0100

    hub and docker

--- makeitso/unix-tooling.sh
+++ makeitso/unix-tooling.sh
@@ -52,6 +52,26 @@ else
 	BASHIT_STATUS="Bash It: Already Installed"
 fi
 
+# FZF Setup -----------------------------------------------------------------------------
+if [ ! -d ~/.hub ]; then
+	HUB_STATUS="Hub:$BOLD Installed $NORMAL"
+
+	git clone https://github.com/github/hub.git ~/.hub && cd ~/.hub
+else
+	HUB_STATUS="Hub: Already Installed"
+fi
+
+# Docker Setup -----------------------------------------------------------------------------
+if [ ! -d ~/.docker ]; then
+	DOCKER_STATUS="Docker:$BOLD Installed $NORMAL"
+
+	wget https://download.docker.com/linux/static/stable/x86_64/docker-17.06.0-ce.tgz
+	tar -xzvf docker-*.tgz
+	rm -rf docker-*.tgz
+else
+	DOCKER_STATUS="Docker: Already Installed"
+fi
+
 # Write Success' -----------------------------------------------------------------------
 # --------------------------------------------------------------------------------------
 
@@ -59,9 +79,11 @@ echo "Tooling Installs"
 echo "--- $NPM_GLOBAL_STATUS"
 echo "--- $RBENV_STATUS"
 echo "--- $RBUILD_STATUS"
+echo "--- $DOCKER_STATUS"
 echo "--- $FZF_STATUS"
 echo "--- $VUNDLE_STATUS"
 echo "--- $BASHIT_STATUS"
+echo "--- $HUB_STATUS"
 echo "Done"
 
 . ~/.bashrc >&-
