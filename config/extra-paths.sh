ec30e2efaf932aaf60515d459aaa43b005e125b7
Jonjoe Whitfield <me@jonjoe.io>
2017-08-19 12:27:59 +0100

    break up generator files

--- config/extra-paths.sh
+++ config/extra-paths.sh
@@ -9,7 +9,6 @@ BIN_PATH="~/.bin"
 RBENV_PATH="~/.rbenv/bin"
 NPM_GLOBAL_PATH="~/.npm-global/bin"
 SBIN_PATH="/usr/local/sbin"
-DOT_LOCAL_PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 WIN_DOCKER_PATH="/mnt/c/Program Files/Docker/Docker/resources/bin"
 BASHER_PATH="~/.basher/bin"
 
@@ -20,7 +19,6 @@ PATH_ARRAY=(
   $RBENV_PATH
   $NPM_GLOBAL_PATH
   $SBIN_PATH
-  $DOT_LOCAL_PATH
   $WIN_DOCKER_PATH
   $BASHER_PATH
 )
--- generators/react-generators.sh
+++ generators/react-generators.sh
@@ -3,15 +3,6 @@
 # Helper functions taht generate react code.
 #
 
-# List all available generators  ==================================
-# -----------------------------------------------------------------
-generator-ls() {
-	echo ''
-	echo '---------------------------------------------------------------'
-	echo '  React Stateless Component | generate-rslc [component name]  '
-	echo '_______________________________________________________________'
-}
-
 # Setup a stateless component  ====================================
 # -----------------------------------------------------------------
 generate-rslc() {
