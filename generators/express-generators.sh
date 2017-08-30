3260a2092de02e0d56a582024a0955989299e7b9
Jonjoe Whitfield <me@jonjoe.io>
2017-08-30 23:09:23 +0100

    fixing tabs

--- generators/express-generators.sh
+++ generators/express-generators.sh
@@ -6,8 +6,8 @@
 # Setup a express rest resource module ============================
 # -----------------------------------------------------------------
 generate-errm() {
-  git clone git@github.com:Jonjoe/express-rest-resource-module.git $@
-  cd ./$@
-  rm -rf .git
-  cd ../
+	git clone git@github.com:Jonjoe/express-rest-resource-module.git $@
+	cd ./$@
+	rm -rf .git
+	cd ../
 }
