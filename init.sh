0824ca77c8fc5d99d3f813c5eecf4e5ebda7f957
Jonjoe Whitfield <me@jonjoe.io>
2020-04-10 00:20:20 +0100

    remove defunt features

--- init.sh
+++ init.sh
@@ -1,23 +1,9 @@
 # | Init ==========================================================
 # -----------------------------------------------------------------
-# ...
 
 FILEPATH=$(realpath $0)
 ROOT=$(dirname $FILEPATH)
 
 . $ROOT/makeitso/global-variables.sh
-. $ROOT/makeitso/host-check.sh
-
-hostOSCheck HOST_OS
-
-if [ "$HOST_OS" = "Windows" ]; then
-  . $ROOT/makeitso/windows-config.sh
-fi
-
-if [ "$HOST_OS" = "Ubuntu" ]; then
-  . $ROOT/makeitso/ubuntu-config.sh
-fi
-
 . $ROOT/makeitso/deploy-configs.sh
 . $ROOT/makeitso/unix-tooling.sh
-. $ROOT/makeitso/node-setup.sh
