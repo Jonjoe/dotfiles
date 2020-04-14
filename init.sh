dac6fa2f552662f083f503d0f801b3170f65e027
Jonjoe Whitfield <jonjoe@Jonjoes-MBP.fritz.box>
2020-04-14 01:41:47 +0100

    override zshrc file written by ohmyzsh

--- init.sh
+++ init.sh
@@ -5,10 +5,10 @@ FILEPATH=$(realpath $0)
 ROOT=$(dirname $FILEPATH)
 
 . $ROOT/makeitso/install-brew-tools.sh
+. $ROOT/makeitso/unix-tooling.sh
 . $ROOT/makeitso/deploy-files.sh
 . $ROOT/makeitso/deploy-scripts.sh
 . $ROOT/makeitso/deploy-nvim.sh
-. $ROOT/makeitso/unix-tooling.sh
 
 
 echo " "
