c310c4c7c2a3f969a1ef3a6b97941c409e2fcb61
Jonjoe Whitfield <me@jonjoe.io>
2020-05-02 19:29:17 +0100

    dead configs

--- scripts/fzf-config.sh
+++
@@ -1,6 +0,0 @@
-# FZF Init ========================================================
-# -----------------------------------------------------------------
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
