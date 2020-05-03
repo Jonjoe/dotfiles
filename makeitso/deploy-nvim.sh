eb218cda5a2296337371ba32088cbb755cc4c568
Jonjoe Whitfield <me@jonjoe.io>
2020-05-03 14:59:08 +0100

    control coc settings from repo

--- makeitso/deploy-nvim.sh
+++ makeitso/deploy-nvim.sh
@@ -16,5 +16,6 @@ cd $ROOT
 rm -rf ~/.config/nvim
 mkdir ~/.config/nvim
 cp -a $ROOT/nvim ~/.config
-rm -rf ~/.config/nvim/init.vim
+rm -rf ~/.config/nvim/init.vim ~/.config/nvim/coc-settings.json
 ln -s $ROOT/nvim/init.vim ~/.config/nvim/init.vim
+ln -s $ROOT/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
