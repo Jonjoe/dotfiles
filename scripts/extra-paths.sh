e4fa2143f646b97f3ba0ca3f66ffd00c5ebebb49
Jonjoe Whitfield <me@jonjoe.io>
2020-05-28 20:00:34 +0100

    (fix): properly load pathing

--- scripts/extra-paths.sh
+++ scripts/extra-paths.sh
@@ -7,10 +7,19 @@
 # -----------------------------------------------------------------
 RBENV_PATH="~/.rbenv/bin"
 PYTHON2_PATH="/usr/local/opt/python@2/libexec/bin"
+FLUTTER_PATH="~/.flutter/bin"
 
 # Load into Path array ============================================
 # -----------------------------------------------------------------
 PATH_ARRAY=(
   $RBENV_PATH
   $PYTHON2_PATH
+  $FLUTTER_PATH
 )
+
+
+for P in "${PATH_ARRAY[@]}"
+do
+  export PATH=$P:$PATH
+done
+
